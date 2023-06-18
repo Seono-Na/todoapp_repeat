541bc988be9a8cda6e62bece8330bdb34304b431
Seono-Na <seono5670@gmail.com>
2023-06-19 02:36:30 +0900

    Filter 추가

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
   const [todos, setTodos] = useState([
     { id: "123", text: "개발하기", status: "active" },
     { id: "124", text: "공부하기", status: "active" },
@@ -12,10 +12,12 @@ export default function TodoList() {
     setTodos(todos.map((item) => (item.id === updated.id ? updated : item)));
   const handleDelete = (deleted) =>
     setTodos(todos.filter((item) => item.id !== deleted.id));
+
+  const filtered = getFilteredItem(todos, filter);
   return (
     <section>
       <ul>
-        {todos.map((item) => (
+        {filtered.map((item) => (
           <Todo
             key={item.id}
             todo={item}
@@ -27,4 +29,10 @@ export default function TodoList() {
       <AddTodo onAdd={handleAdd} />
     </section>
   );
+}
+function getFilteredItem(todos, filter) {
+  if (filter === "all") {
+    return todos;
+  }
+  return todos.filter((todo) => todo.status === filter);
 }
